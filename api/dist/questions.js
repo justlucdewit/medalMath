3888386d252d1affcbcc01c164dbbc40be6ed08c
Luc de wit <luc.dewit@hotmail.nl>
2020-04-21 21:23:28 +0200

    worked on question generator

--- api/dist/questions.js
+++ api/dist/questions.js
@@ -1,31 +1,34 @@
 "use strict";
-const generateQuestions = (config, amount) => {
+const generateQuestion = (config) => {
+    let q = { question: "", answer: 0 };
+    for (let i = 0; i < config.numberOfTerms * 2 - 1; i++) {
+        if (i % 2 == 0) {
+            q.question += Math.floor(Math.random() * 10 ** (config.maxDigits - 1));
+        }
+        else {
+            q.question += config.operatorsAllowed[Math.floor(Math.random() * config.operatorsAllowed.length)];
+        }
+    }
+    q.answer = eval(q.question);
+    while (!config.negativesAllowed && q.answer < 0) {
+        q = generateQuestion(config);
+    }
+    return q;
+};
+const generateQuestionArray = (config, amount) => {
     let questions = [];
     for (let q = 0; q < amount; q++) {
-        let ret = {
-            question: "",
-            answer: 0,
-        };
-        for (let i = 0; i < config.numberOfTerms * 2 - 1; i++) {
-            if (i % 2 == 0) {
-                ret.question += Math.floor(Math.random() * 10 ** (config.maxDigits - 1));
-            }
-            else {
-                ret.question += config.operatorsAllowed[Math.floor(Math.random() * config.operatorsAllowed.length)];
-            }
-        }
-        ret.answer = eval(ret.question);
-        questions.push(ret);
+        questions.push(generateQuestion(config));
     }
     return questions;
 };
 const config = {
     maxDigits: 3,
     numberOfTerms: 2,
-    operatorsAllowed: ["+", "-"],
+    operatorsAllowed: ["+", "-", "*", "/"],
     negativesAllowed: false
 };
-const questions = generateQuestions(config, 30);
+const questions = generateQuestionArray(config, 30);
 for (const question of questions) {
     console.log(`${question.question} = ${question.answer}`);
 }
